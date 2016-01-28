3c8a8849cc8b3f033cfd99795ac81cf622789aa3
dorothym <dorothy.v.moore@gmail.com>
2016-01-28 13:10:48 -0500

    working on page.spec.js

--- test/page.spec.js
+++ test/page.spec.js
@@ -1,26 +1,33 @@
-require('../models/');
-require('../routes/');
+var models = require('../models/');
+// var routes = require('../routes/');
+var Page = models.Page;
+// var User = models.User;
 
-describe('Testing Page schema and model', function () {
+describe('Page model', function() {
 
-	// create new instance of Page with test data
-
-    describe('Test new instance - invalid data', function () {
-
+    describe('Validations', function() {
     	// create new instance - invalid 
     	beforeEach(function(){
 	        var page1 = new Page({
 	        	// has no title
 	        	content: 'Test content'
-	        })
+	        });
 	        var page2 = new Page({
 	        	title: 'Has a title'
 	        	// no content
-	        })
-	    })
+	        });
+	    });
 
-        xit('test if user entered title', function () {});
-        xit('test if user entered content', function () {});
+        it('errors without title', function(done) {
+
+        	page1.validate(function(err) {
+        		console.error(err);
+                err.should.exist;
+                done();
+        	})
+
+        });
+        xit('errors without content', function() {});
 
     });
     describe('Test new instance - valid data', function () {
@@ -28,7 +35,7 @@ describe('Testing Page schema and model', function () {
     	// create new instance - valid 
     	beforeEach(function(){
 	        var page2 = new Page({
-	        	title: 'Test title 2'
+	        	title: 'Test title 2',
 	        	content: 'Test content'
 	        })
 	    })
@@ -38,4 +45,45 @@ describe('Testing Page schema and model', function () {
 
     });
 });
-describe('A different category', function () {});
+
+
+
+/*
+
+
+
+describe('Page model', function() {
+
+    describe('Validations', function() {
+        xit('errors without title', function() {});
+        xit('errors without content', function() {});
+    });
+
+    describe('Statics', function() {
+        describe('findByTag', function() {
+            xit('gets pages with the search tag', function() {});
+            xit('does not get pages without the search tag', function() {});
+        });
+    });
+
+    describe('Methods', function() {
+        describe('findSimilar', function() {
+            xit('never gets itself', function() {});
+            xit('gets other pages with any common tags', function() {});
+            xit('does not get other pages without any common tags', function() {});
+        });
+    });
+
+    describe('Virtuals', function() {
+        describe('route', function() {
+            xit('returns the url_name prepended by "/wiki/"', function() {});
+        });
+    });
+
+    describe('Hooks', function() {
+        xit('it sets urlTitle based on title before validating', function() {});
+    });
+
+});
+
+*/
